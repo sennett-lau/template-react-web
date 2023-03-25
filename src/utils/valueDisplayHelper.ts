8e67817672f509993426c4aa2e6dd94bac4fbaf5
Sennett Lau <laub1199@gmail.com>
2023-03-26 02:20:20 +0800

    added more utils

--- src/utils/valueDisplayHelper.ts
+++ src/utils/valueDisplayHelper.ts
@@ -1,17 +1,10 @@
-export const addressDisplay = (address, l = 8, r = -6) => {
+export const addressDisplay = (address:string, l = 8, r = -6): string => {
   if (typeof address !== 'string') return address
   return address.slice(0, l) + '...' + address.slice(r)
 }
 
-export const numberDisplay = (value) => {
+export const numberDisplay = (value: number | string): string => {
   if (typeof value === 'number') value = value.toFixed(2)
   else if (typeof value === 'string') value = parseFloat(value).toFixed(2)
   return value?.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')
-}
-
-export const toTitleCase = (str) => {
-  return str
-    .split('_')
-    .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
-    .join(' ')
 }
