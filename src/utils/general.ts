57937615b4db2dde4cf3c73d199d47a7778a4ad5
Sennett Lau <laub1199@gmail.com>
2023-04-01 21:32:48 +0800

    added explanation for objMapToList and keyObjMapToList

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -11,6 +11,9 @@ export const listToObjMap = <T>(list: IDynamicObject[], key: string): IGenericOb
 }
 
 export const objMapToList = <T>(obj: IGenericObject<T>): T[] => {
+  // this function will ignore the key and only push the value to the list
+  // { key1: value1, key2: value2 } => [value1, value2]
+
   const list: T[] = []
 
   Object.keys(obj).forEach((key: string) => {
@@ -21,6 +24,10 @@ export const objMapToList = <T>(obj: IGenericObject<T>): T[] => {
 }
 
 export const keyObjMapToList = <T>(obj: IGenericObject<T>, keyName: string, valueName: string): { [key: string]: IGenericObject<T> }[] => {
+  // this function will push the key and value to the list
+  // keyName = 'key', valueName = 'value'
+  // { key1: value1, key2: value2 } => [{ key: key1, value: value1 }, { key: key2, value: value2 }]
+
   const list= []
 
   Object.keys(obj).forEach((key: string) => {
